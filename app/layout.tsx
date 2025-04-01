075050cddd455b26d79b4ddd3acbdeb19aad5eb0
Gaa <gaa.song@gmail.com>
2025-04-01 13:36:40 +1100

    add tools

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Montserrat } from "next/font/google"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css"
 import "../styles/navbar.css"
 
@@ -41,7 +43,11 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👩🏻‍💻</text></svg>" />
         <meta name="description" content="Frontend Engineer & Product Owner" />
       </head>
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        {children}
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
   )
 }
